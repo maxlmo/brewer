b28691c790fd914cc070aa76484c4fa221a44da8
maxlmo <max.moeller55@gmail.com>
2020-05-10 13:07:13 +0200

    Implement current temperature method

--- src/main.py
+++ src/main.py
@@ -2,14 +2,12 @@ import time, sys
 from temperatureSensor import TemperatureSensor
     
 sensorPath = "/sys/bus/w1/devices/28-0119136e048d/w1_slave"
-tempSensor = TemperatureSensor(sensorPath)
+tempSensor = TeperatureSensor(sensorPath)
 
 try:
-    x = 1
-    while x < 10:
-        print(time.strftime('%H:%M:%S') +  " temp: " + str(tempSensor.readTempLines()[0]) + " °C")
+    while True:
+        print(time.strftime('%H:%M:%S') +  " temp: " + tempSensor.getCurrentTemperature() + " °C")
         time.sleep(1)
-        x++ 
 except KeyboardInterrupt:
     print("key interrupt")
 except Exception as e:
--- src/temperatureSensor.py
+++ src/temperatureSensor.py
@@ -3,21 +3,27 @@ class TemperatureSensor:
     def __init__(self, path):
         self.path = path 
 
-    def readTemperature(self):
-        f = open(sensorName, 'r')
-        lines = f.readlines()
-        f.close()
-        return lines
+    def getCurrentTemperature(self):
+        file = open(self.path, 'r')
+        lines = file.readlines()
+        file.close()
+        print(lines)
 
-    def readTempLines(self):
-        lines = self.readTempSensor(self.path)
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = readTempSensor(sensorName)
-        temperaturStr = lines[1].find('t=')
-        if temperaturStr != -1:
-            tempData = lines[1][temperaturStr+2:]
-            tempCelsius = float(tempData) / 1000.0
-            tempKelvin = 273 + float(tempData) / 1000
-            tempFahrenheit = float(tempData) / 1000 * 9.0 / 5.0 + 32.0
-            return [tempCelsius, tempKelvin, tempFahrenheit]
+
+
+
+
+
+
+    # def readTempLines(self):
+    #     lines = self.readTemperature(self.path)
+    #     while lines[0].strip()[-3:] != 'YES':
+    #         time.sleep(0.2)
+    #         lines = readTempSensor(sensorName)
+    #     temperaturStr = lines[1].find('t=')
+    #     if temperaturStr != -1:
+    #         tempData = lines[1][temperaturStr+2:]
+    #         tempCelsius = float(tempData) / 1000.0
+    #         tempKelvin = 273 + float(tempData) / 1000
+    #         tempFahrenheit = float(tempData) / 1000 * 9.0 / 5.0 + 32.0
+    #         return [tempCelsius, tempKelvin, tempFahrenheit]
