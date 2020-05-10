f09b4cd6144cc90b7748c10c4a20086f72b260ac
maxlmo <max.moeller55@gmail.com>
2020-05-10 14:37:42 +0200

    Display temp on led display

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
 import time, sys
+
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.legacy import text, show_message
+from luma.led_matrix.device import max7219
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from luma.core.virtual import viewport
+
 from temperatureSensor import TemperatureSensor
     
 sensorPath = "/sys/bus/w1/devices/28-0119136e048d/w1_slave"
 tempSensor = TemperatureSensor(sensorPath)
 
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, width=32, height=8, block_orientation=-90)
+
 try:
     while True:
-        print(time.strftime('%H:%M:%S') +  " temp: " + str(tempSensor.getCurrentTemperature()) + " °C")
+        tempstr = str(tempSensor.getCurrentTemperature()) + " °C"
+        print(time.strftime('%H:%M:%S') +  " temp: " + tempstr)
         time.sleep(1)
+        
+        with canvas(device) as draw:
+            text(draw, (1, 1), tempstr, fill="white", font=proportional(CP437_FONT))
+
 except KeyboardInterrupt:
     print("key interrupt")
 except Exception as e:
@@ -16,3 +32,4 @@ except Exception as e:
 finally:
     print("exit")
     sys.exit(0)
+    
