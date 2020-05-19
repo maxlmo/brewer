86d8ef2a3b135f42e60b778d7e8b464cc3e0be34
maxlmo <max.moeller55@gmail.com>
2020-05-19 22:58:25 +0200

    Implement initial thread

--- src/app.py
+++ src/app.py
@@ -3,40 +3,76 @@ from flask import Flask, render_template, jsonify
 from flask_injector import FlaskInjector
 from injector import inject
 from flask import request
+import threading
+import atexit
+import time
 
 from src.dependency_injection import configure
+from src.sensor.fake_temperature_sensor import FakeTemperatureSensor
 from src.sensor.temperature_sensor import TemperatureSensor
-
-app: Flask = Flask(__name__)
-app.config.update(TEMPLATES_AUTO_RELOAD=True)
-app._static_folder = os.path.abspath("templates/static/")
 
 min = 50
 max = 100
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+
+POOL_TIME = 5
+
+# variables that are accessible from anywhere
+current_temperature = 0
+# lock to control access to variable
+dataLock = threading.Lock()
+# thread handler
+thread = threading.Thread()
 
 
-@inject
-@app.route('/temp')
-def get_temperature(ts: TemperatureSensor) -> str:
-    return str(ts.read_temperature())
+def create_app():
+    app: Flask = Flask(__name__)
+    app.config.update(TEMPLATES_AUTO_RELOAD=True)
+    app._static_folder = os.path.abspath("templates/static/")
+
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+
+    @app.route('/range', methods=['POST'])
+    def set_range_value():
+        global min, max
+        min = request.get_json()["min"]
+        max = request.get_json()["max"]
+        return ""
+
+    @app.route('/range', methods=['GET'])
+    def send_range_value():
+        return jsonify({"min": min, "max": max})
+
+    @app.route("/temp")
+    def temp():
+        global current_temperature
+        return str(current_temperature)
+
+    def stop_thread():
+        global thread
+        thread.cancel()
+
+    def loop():
+        global current_temperature
+        temp_sensor = FakeTemperatureSensor()
+        while True:
+            current_temperature = temp_sensor.read_temperature()
+            time.sleep(POOL_TIME)
+
+    def start_threat():
+        # Do initialisation stuff here
+        global thread
+        # Create your thread
+        thread = threading.Timer(POOL_TIME, loop, ())
+        thread.start()
+
+    start_threat()
+    FlaskInjector(app=app, modules=[configure])
+    atexit.register(stop_thread)
+    return app
 
 
-@app.route('/range', methods=['POST'])
-def set_range_value():
-    global min, max
-    min = request.get_json()["min"]
-    max = request.get_json()["max"]
-    return ""
-
-
-@app.route('/range', methods=['GET'])
-def send_range_value():
-    return jsonify({"min": min, "max": max})
-
-
-FlaskInjector(app=app, modules=[configure])
-app.run('0.0.0.0')
+if __name__ == '__main__':
+    create_app().run(debug=True, host='0.0.0.0')
